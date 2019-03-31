227bbedd7b1e5ad96012335c35657fae9d51ef42
zaypen <zaypen@gmail.com>
2019-03-31 14:18:24 +0800

    Remove alias of emacs from MacPorts

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -1,6 +1,1 @@
 alias sudo="sudo -s"
-
-if which /Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs >/dev/null
-  alias emacs=/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs
-  alias e="emacs -nw"
-end
