f1d5853f310e550700a3c897ece9e8f3c09e28a3
zaypen <zaypen@gmail.com>
2019-10-02 15:55:08 +0800

    Fix wrapped ls

--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -2,6 +2,6 @@ function ls --wraps lsd --description 'Alias for lsd'
     if type -q lsd >/dev/null
         lsd $argv --icon never
     else
-        ls $argv
+        command ls $argv
     end
 end
