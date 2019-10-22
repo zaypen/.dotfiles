fd15f46ca12ba5b2946ad05defb3d917e8b3484a
zaypen <zaypen@gmail.com>
2019-10-22 22:04:22 +0800

    Disable creation of lock file

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -318,6 +318,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq create-lockfiles nil)
   )
 
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
