30f2f96fe9e83465778fd363e176afeccf003520
zaypen <zaypen@gmail.com>
2019-10-27 23:47:47 +0800

    Handle history commands with backslash gracefully

--- fish/.config/fish/functions/hf.fish
+++ fish/.config/fish/functions/hf.fish
@@ -1,7 +1,8 @@
 function hf --wraps history --description 'Show and manipulate command history and fuzzy search it, glhf'
     if type -q fzy >/dev/null
-        commandline -r (history $argv | fzy || echo '')
+        commandline -r (history $argv -z | sed -z -r s/\\\\\\n// | tr "\0" "\n" | fzy || echo "")
     else
         history $argv
     end
 end
+
