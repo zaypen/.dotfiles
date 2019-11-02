ff9f071c5e66f427140911150fed0c77d7fcee60
zaypen <zaypen@gmail.com>
2019-11-03 02:30:54 +0800

    Suppress errors from argparse

--- fish/.config/fish/functions/emacsclient.fish
+++ fish/.config/fish/functions/emacsclient.fish
@@ -1,6 +1,6 @@
 function emacsclient --wraps emacsclient --description 'emacsclient wrapper that starts emacs daemon'
     set -l options $argv
-    argparse "s/socket-name=" -- $argv
+    argparse "s/socket-name=" -- $argv >/dev/null 2>/dev/null
     if set --query $_flag_s
         if not command emacsclient -e t >/dev/null 2>/dev/null
             screen emacs --daemon
