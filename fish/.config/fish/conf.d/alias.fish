2250b6ac6d0e7594b14f32d0bee2511d5d0e3657
zaypen <zaypen@gmail.com>
2019-11-12 23:23:40 +0800

    Remove alias of pio as it's originally provided

--- fish/.config/fish/conf.d/alias.fish
+++ fish/.config/fish/conf.d/alias.fish
@@ -5,6 +5,3 @@ alias g="git"
 command -v emacsclient >/dev/null; and \
 alias ec="emacsclient"; \
 alias eg="emacsclient -s graphic"
-
-command -v platformio >/dev/null; and \
-alias pio="platformio"
