6a240449cab7092e2d58ea3e9a2df9d518a69bfe
zaypen <zaypen@gmail.com>
2020-06-20 00:10:31 +0800

    Apply direnv on fish_prompt

--- fish/.config/fish/conf.d/direnv.fish
+++ fish/.config/fish/conf.d/direnv.fish
@@ -1,5 +1,5 @@
 # Modified from direnv hook fish >~/.config/fish/conf.d/direnv.fish
 
-function __direnv_export_eval --on-event fish_prompt
+function __direnv_export_eval_prompt --on-event fish_prompt
 	direnv export fish | source
 end
