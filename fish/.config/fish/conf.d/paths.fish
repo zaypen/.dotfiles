6f174bb9dd8d8ca41e8a9d06143286411404167f
zaypen <zaypen@gmail.com>
2021-11-29 16:46:49 +0800

    Improve loading of virtual envs

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -1,4 +1,9 @@
 # Append bin of each virtual env under ~/.venvs
-ls -1d $HOME/.venvs/* | while read -l venv
-    set -x PATH $PATH $HOME/.venvs/$venv/bin
+
+if [ -d $HOME/.venvs ]
+  if [ (ls -1d $HOME/.venvs/* | wc -l) -gt 0 ]
+    ls -1d $HOME/.venvs/* | while read -l venv
+      set -x PATH $PATH $venv/bin
+    end
+  end
 end
