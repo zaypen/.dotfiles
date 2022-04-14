569aeb7b4bc1c340df24148e30fcc900c11f0f09
zaypen <zaypen@gmail.com>
2022-04-14 15:49:19 +0800

    Call direnv only if it's present

--- fish/.config/fish/conf.d/direnv.fish
+++ fish/.config/fish/conf.d/direnv.fish
@@ -1,5 +1,5 @@
 # Modified from direnv hook fish >~/.config/fish/conf.d/direnv.fish
 
 function __direnv_export_eval_prompt --on-event fish_prompt
-	direnv export fish | source
+  type -q direnv && direnv export fish | source
 end
