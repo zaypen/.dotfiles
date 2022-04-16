49dd0eff58b905dde7309498fad1344eda0e19a6
zaypen <zaypen@gmail.com>
2022-04-17 05:05:31 +0800

    Add conf and function of flutter

---
+++ fish/.config/fish/conf.d/flutter.fish
@@ -0,0 +1,12 @@
+function __fish_flutter_enable_mirror
+    set -gx PUB_HOSTED_URL https://pub.flutter-io.cn
+    set -gx FLUTTER_STORAGE_BASE_URL https://storage.flutter-io.cn
+    set -gx FLUTTER_GIT_URL https://mirrors.tuna.tsinghua.edu.cn/git/flutter-sdk.git
+end
+
+function __fish_flutter_disable_mirror
+    set -ge PUB_HOSTED_URL FLUTTER_STORAGE_BASE_URL FLUTTER_GIT_URL
+end
+
+# Enable mirror by default
+__fish_flutter_enable_mirror
