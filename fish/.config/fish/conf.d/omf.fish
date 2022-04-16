c3b6299804bce115fa51298781a0a076032b21cf
zaypen <zaypen@gmail.com>
2022-04-17 04:58:28 +0800

    Tweak omf

--- fish/.config/fish/conf.d/omf.fish
+++ fish/.config/fish/conf.d/omf.fish
@@ -1,9 +1,11 @@
-if status --is-interactive
-  # Path to Oh My Fish install.
-  set -q XDG_DATA_HOME
-    and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-    or set -gx OMF_PATH "$HOME/.local/share/omf"
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-  # Load Oh My Fish configuration.
-  source $OMF_PATH/init.fish
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+if ! status --is-interactive
+  set -g theme_powerline_fonts no
 end
