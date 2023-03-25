a55eeb9d43078ada021afa15848965b52079f42e
zaypen <zaypen@gmail.com>
2023-03-25 21:21:20 +0800

    Make it flexible

--- fish/.config/fish/conf.d/paths.fish
+++ fish/.config/fish/conf.d/paths.fish
@@ -3,6 +3,6 @@
 if [ -d $HOME/.venvs ]
   and [ (ls -1d $HOME/.venvs/* | wc -l) -gt 0 ]
   ls -1d $HOME/.venvs/* | while read -l venv
-    fish_add_path --append  $venv/bin
+    fish_add_path --append $venv/bin
   end
 end
