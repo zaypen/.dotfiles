af311b756e1081e3549ae0f1291b8fde4caf1b43
zaypen <zaypen@gmail.com>
2025-01-26 04:16:11 +0800

    Update emacs aliases

--- fish/.config/fish/conf.d/editor.fish
+++ fish/.config/fish/conf.d/editor.fish
@@ -1,4 +1,4 @@
 if command -q emacs
-    set -x VISUAL emacsclient -t -socket-name=cli -a emacs
+    set -x VISUAL emacsclient -s term -t -a emacs
     set -x EDITOR vi
 end
--- fish/.config/fish/conf.d/emacs.fish
+++ fish/.config/fish/conf.d/emacs.fish
@@ -1,6 +1,6 @@
 if test -d /Applications/MacPorts/Emacs.app
     fish_add_path --append /Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs/bin
-    alias e="emacsclient -s term -t -a ''"
-    alias eq="emacsclient -s term -e \"(kill-emacs)\""
+    alias emacs=Emacs
+    alias e="emacsclient -s term -t -a emacs"
     alias eg="emacsclient -c -n -a emacs"
 end
